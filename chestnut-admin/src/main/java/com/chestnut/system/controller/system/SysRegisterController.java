0b92f56dca59f2494308fb77894a6d22f9e57ed1
yeshaokang <ysk202092@163.com>
2023-05-10 14:43:42 +0800

    变更文件地址

---
+++ chestnut-admin/src/main/java/com/chestnut/system/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.chestnut.system.controller.system;
+
+import com.chestnut.common.core.controller.BaseController;
+import com.chestnut.common.core.domain.AjaxResult;
+import com.chestnut.common.core.domain.model.RegisterBody;
+import com.chestnut.common.utils.StringUtils;
+import com.chestnut.manager.biz.service.SysRegisterService;
+import com.chestnut.system.service.ISysConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 注册验证
+ * 
+ * @author chestnut
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
